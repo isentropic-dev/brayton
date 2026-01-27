34489ea2f7e347cd7b27199c0c9cd57f7f4256dc
John Dyreby <john@isentropic.dev>
2026-01-27 10:26:17 -0600

    Add IsentropicEfficiency wrapper type

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,9 @@ mod error;
 mod operating_point;
 mod solution;
 
-pub use config::{Config, HxConfig, PressureDrop, RecuperatorConfig, TurboConfig};
+pub use config::{
+    Config, HxConfig, IsentropicEfficiency, PressureDrop, RecuperatorConfig, TurboConfig,
+};
 pub use error::Error;
 pub use operating_point::OperatingPoint;
 pub use solution::{CycleStates, Solution};
